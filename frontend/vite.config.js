c92408a59c76f4dfc9648edd6d1aa90d922a1965
Harshit Mestry <106076610+Harshitttttttt@users.noreply.github.com>
2024-07-02 16:09:04 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://chatlore-api.onrender.com/",
+        target: "https://chatlore-api.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\//, ""),
       },
