389d096bfbbd63033e3f60e346f46f3c7495d698
Harshit Mestry <106076610+Harshitttttttt@users.noreply.github.com>
2024-07-02 16:11:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chatlore-app.vercel.app/",
+    origin: "https://chatlore-app.vercel.app",
   },
 });
 
